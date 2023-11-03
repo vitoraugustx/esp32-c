88068a5245ef79a08f49a9c12d96885a3f1ebccb
vitoraugustx <vitor.arp2010@gmail.com>
2023-11-03 18:20:34 -0300

    modificações na média

--- MAX30102_bpm/MAX30102_bpm.ino
+++ MAX30102_bpm/MAX30102_bpm.ino
@@ -5,7 +5,7 @@
 
 MAX30105 particleSensor;
 
-const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+const byte RATE_SIZE = 8; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
